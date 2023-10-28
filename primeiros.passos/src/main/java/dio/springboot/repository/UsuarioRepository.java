724e9e637cc489b1d2cecd868c9704d42625ad55
leticiamp <leticia.mpinheiro16@gmail.com>
2023-10-28 16:07:54 -0300

    swagger

---
+++ primeiros.passos/src/main/java/dio/springboot/repository/UsuarioRepository.java
@@ -0,0 +1,39 @@
+package dio.springboot.repository;
+
+import dio.springboot.handler.BusinessException;
+import dio.springboot.model.Usuario;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UsuarioRepository {
+
+    public void save(Usuario usuario){
+        if(usuario.getLogin()==null) {
+            throw new BusinessException("O campo login é obrigatório!");
+        }
+        System.out.println("SAVE - Recebendo o usuário na camada de repositório");
+        System.out.println(usuario);
+    }
+    public void update(Usuario usuario){
+        System.out.println("UPDATE - Recebendo o usuário na camada de repositório");
+        System.out.println(usuario);
+    }
+    public void remove(Integer id){
+        System.out.println(String.format("DELETE/id - Recebendo o id: %d para excluir um usuário", id));
+        System.out.println(id);
+    }
+    public List<Usuario> listAll(){
+        List<Usuario> usuarios = new ArrayList<>();
+        usuarios.add(new Usuario("gleyson","password"));
+        usuarios.add(new Usuario("frank","masterpass"));
+        return usuarios;
+    }
+    public Usuario finById(Integer id){
+        System.out.println(String.format("GET/id - Recebendo o id: %d para localizar um usuário", id));
+        return new Usuario("gleyson","password");
+    }
+
+}
